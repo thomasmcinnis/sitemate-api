742818ecf2fd66c24f46af5b5a1e22adc11ce284
Thomas McInnis <hey@thomasmcinnis.com>
2024-06-13 12:20:09 +1000

    Show list of issues on frontent

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import {
+  getAllIssues,
+  getIssueById,
+  deleteIssue,
+  updateIssue,
+} from './services/api';
 import './App.css';
 
 function App() {
-  return <div>This is my app</div>;
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    fetchIssues();
+  }, []);
+
+  const fetchIssues = async () => {
+    const issues = await getAllIssues();
+    setIssues(issues);
+  };
+  return (
+    <div>
+      <h1>Issues</h1>
+      <ul>
+        {issues.map((issue) => (
+          <li key={issue.id}>
+            {issue.title}: {issue.description}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
