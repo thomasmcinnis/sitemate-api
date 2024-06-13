45e96ce0e97544614c792963453ee367ebe7c3b4
Thomas McInnis <hey@thomasmcinnis.com>
2024-06-13 12:24:14 +1000

    Add ability to add issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import {
   getIssueById,
   deleteIssue,
   updateIssue,
+  createIssue,
 } from './services/api';
 import './App.css';
 
 function App() {
   const [issues, setIssues] = useState([]);
+  const [newIssue, setNewIssue] = useState({ title: '', description: '' });
 
   useEffect(() => {
     fetchIssues();
@@ -18,6 +20,13 @@ function App() {
     const issues = await getAllIssues();
     setIssues(issues);
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await createIssue(newIssue);
+    fetchIssues();
+    setNewIssue({ title: '', description: '' });
+  };
+
   return (
     <div>
       <h1>Issues</h1>
@@ -28,6 +37,23 @@ function App() {
           </li>
         ))}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          value={newIssue.title}
+          onChange={(e) => setNewIssue({ ...newIssue, title: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          value={newIssue.description}
+          onChange={(e) =>
+            setNewIssue({ ...newIssue, description: e.target.value })
+          }
+        />
+        <button type="submit">Add Issue</button>
+      </form>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,17 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import config from './util/config.js';
-// import middleware from './util/middleware.js';
 import issueRouter from './controllers/issues.js';
-// import logger from './util/logger.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use(middleware.requestLogger);
 app.use('/api/issues', issueRouter);
-
-// app.use(middleware.unknownEndpoint);
-// app.use(middleware.errorHandler);
 
 export default app;
